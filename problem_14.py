8781a024d9ff6295c821f8f31659c67887b9af5e
Vahit Buğra Yeşilkaynak <mrgranddy@Vahits-MacBook-Air.local>
2021-10-27 12:59:32 +0200

    auto black formatting

--- problem_14.py
+++ problem_14.py
@@ -1,20 +1,22 @@
 mem = {}
 
+
 def recursive_chain(x):
     if x == 1:
         return 1
     if x in mem:
         return mem[x]
     if x % 2 == 0:
-        mem[x] = 1 + recursive_chain(x//2)
+        mem[x] = 1 + recursive_chain(x // 2)
     else:
-        mem[x] = 1 + recursive_chain(3*x+1)
+        mem[x] = 1 + recursive_chain(3 * x + 1)
     return mem[x]
 
+
 m = (0, None)
-for i in range(1,int(1e6)):
+for i in range(1, int(1e6)):
     c = recursive_chain(i)
     if c > m[0]:
         m = (c, i)
-        
+
 print(m[1])
