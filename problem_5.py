4037ecb726cde5c466536a29805101ec3d220555
Vahit Buğra Yeşilkaynak <mrgranddy@Vahits-MacBook-Air.local>
2021-10-27 06:13:36 +0200

    Problems 5-7

---
+++ problem_5.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def find_primes(n):
+    A = np.ones(n+1, dtype="int32")
+    i = 2
+    while i <= n ** 0.5:
+        if A[i] == 1:
+            j = i ** 2
+            while j <= n:
+                A[j] = 0
+                j += i
+        i += 1
+    return np.where(A == 1)[0][2:]
+
+n = 20
+primes = find_primes(n)
+m = 1
+for prime in primes:
+    power = int(np.log(n) // np.log(prime))
+    m *= prime ** power
+    
+print(m)
---
+++ problem_7.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def find_nth_prime(idx):
+    n = 999999
+    A = np.ones(n+1, dtype="int32")
+    i = 2
+    cnt = 0
+    while i <= n ** 0.5:
+        if A[i] == 1:
+            j = i ** 2
+            while j <= n:
+                A[j] = 0
+                j += i
+            cnt += 1
+        if cnt == idx:
+            break
+        i += 1
+    return np.where(A == 1)[0][1+idx]
+
+print( find_nth_prime(10001) )
